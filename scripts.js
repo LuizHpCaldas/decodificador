1b90a23d3534cc74637101cc6ded1c28bb0911ed
Usuario <luizhpcaldas@gmail.com>
2024-07-04 13:39:48 -0300

    ajustes finais

--- scripts.js
+++ scripts.js
@@ -8,7 +8,8 @@ function criptografar() {
         .replace(/a/g, 'ai')
         .replace(/o/g, 'ober')
         .replace(/u/g, 'ufat');
-    document.getElementById('output-text').value = textoCriptografado;
+    document.getElementById('input-text').value = ''; // Limpa o campo de entrada
+    mostrarResultado(textoCriptografado);
 }
 
 function descriptografar() {
@@ -19,37 +20,24 @@ function descriptografar() {
         .replace(/ai/g, 'a')
         .replace(/ober/g, 'o')
         .replace(/ufat/g, 'u');
-    document.getElementById('output-text').value = textoDescriptografado;
+    document.getElementById('input-text').value = ''; // Limpa o campo de entrada
+    mostrarResultado(textoDescriptografado);
 }
 
-function copiarTexto() {
-    let texto = document.getElementById('output-text');
-    texto.select();
-    document.execCommand('copy');
-    alert('Texto copiado para a área de transferência!');
-}
-// scripts.js
+function mostrarResultado(texto) {
+    let outputText = document.getElementById('output-text');
+    let outputContainer = document.getElementById('output-container');
+    let copyButton = document.getElementById('copy-button');
 
-function criptografar() {
-    let texto = document.getElementById('input-text').value;
-    let textoCriptografado = texto
-        .replace(/e/g, 'enter')
-        .replace(/i/g, 'imes')
-        .replace(/a/g, 'ai')
-        .replace(/o/g, 'ober')
-        .replace(/u/g, 'ufat');
-    document.getElementById('output-text').value = textoCriptografado;
-}
+    outputText.value = texto;
 
-function descriptografar() {
-    let texto = document.getElementById('input-text').value;
-    let textoDescriptografado = texto
-        .replace(/enter/g, 'e')
-        .replace(/imes/g, 'i')
-        .replace(/ai/g, 'a')
-        .replace(/ober/g, 'o')
-        .replace(/ufat/g, 'u');
-    document.getElementById('output-text').value = textoDescriptografado;
+    if (texto) {
+        outputContainer.classList.add('show-output');
+        copyButton.classList.add('show-copy');
+    } else {
+        outputContainer.classList.remove('show-output');
+        copyButton.classList.remove('show-copy');
+    }
 }
 
 function copiarTexto() {
